3e098233762db09647c77d3b5ab0a24edcc8350c
beratazgun <52507194+beratazgun@users.noreply.github.com>
2022-03-10 19:18:37 +0300

    Delete hangman_words.py

--- hangman_words.py
+++
@@ -1,3 +0,0 @@
-word_list = ['kanepe', 'kitap', 'bardak', 'araba', 'su', 'bilgisayar', 'çorap', 'kablo', 'uçak', 'telefon',
-            'yılan', 'şişe', 'kış', 'yıl', 'kırmızı', 'mavi', 'yeşil', 'siyah', 'beyaz', 'kırmızı', 'mavi',
-            'yeşil', 'armut', 'karpuz', 'kavun', 'erik', 'şeftali', 'nektari', 'otobüs', 'zırh', 'kılıç']
