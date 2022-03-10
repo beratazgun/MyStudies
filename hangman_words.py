1fe3d4b35a04b24259b5c39545fd7ec5613309e5
beratazgun <52507194+beratazgun@users.noreply.github.com>
2022-03-10 19:05:00 +0300

    Add files via upload

---
+++ hangman_words.py
@@ -0,0 +1,3 @@
+word_list = ['kanepe', 'kitap', 'bardak', 'araba', 'su', 'bilgisayar', 'çorap', 'kablo', 'uçak', 'telefon',
+            'yılan', 'şişe', 'kış', 'yıl', 'kırmızı', 'mavi', 'yeşil', 'siyah', 'beyaz', 'kırmızı', 'mavi',
+            'yeşil', 'armut', 'karpuz', 'kavun', 'erik', 'şeftali', 'nektari', 'otobüs', 'zırh', 'kılıç']
